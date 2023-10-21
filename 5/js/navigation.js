cbe53027de2fc94bac572feaafe0ce709032d214
Keks <keks@htmlacademy.ru>
2023-10-21 16:04:15 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/navigation.js
@@ -0,0 +1,24 @@
+let navigationMenu = document.querySelector('.navigation');
+let navigationToogle = document.querySelector('.main-header__navigation-toggle');
+
+navigationMenu.classList.remove('navigation--nojs');
+navigationToogle.classList.remove('main-header__navigation-toggle--opened');
+navigationToogle.classList.add('main-header__navigation-toggle--closed');
+
+navigationToogle.addEventListener('click', function() {
+
+  if(navigationMenu.classList.contains('navigation--closed')) {
+    navigationMenu.classList.remove('navigation--closed');
+    navigationMenu.classList.add('navigation--opened');
+
+    navigationToogle.classList.remove('main-header__navigation-toggle--closed');
+    navigationToogle.classList.add('main-header__navigation-toggle--opened');
+  }
+  else {
+    navigationMenu.classList.remove('navigation--opened');
+    navigationMenu.classList.add('navigation--closed');
+
+    navigationToogle.classList.remove('main-header__navigation-toggle--opened');
+    navigationToogle.classList.add('main-header__navigation-toggle--closed');
+  }
+});
